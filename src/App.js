52c7e72a4a569164609597164d04202557d85d26
Johnson Zead <johnsonzead@gmail.com>
2019-03-22 17:42:11 +1100

    Test buttom function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './App.css'
 
 class App extends Component {
+  handleClick(e) {
+    const body = document.querySelector('body')
+    body.style.backgroundColor = '#79b4c4'
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>GreenAir Air Pollution Tracker</h1>
+        <button onClick={this.handleClick}>Change the background color</button>
       </div>
     );
   }
